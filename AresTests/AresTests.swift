b6f9f9cbfb638b84e12499e5fd4458b350f058c9
Joao Pires <mail@joaopires.com>
2025-01-04 13:47:53 +0000

    Initial Commit & Project Creation

---
+++ AresTests/AresTests.swift
@@ -0,0 +1,16 @@
+//
+//  AresTests.swift
+//  AresTests
+//
+//  Created by Joao Pires on 04/01/2025.
+//
+
+import Testing
+
+struct AresTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
